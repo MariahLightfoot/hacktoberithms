5f83a9c215097b8bb3f61dbf9dc72fb09379a35d
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-10-16 18:04:02 -0500

    base instructions and file

---
+++ javascript/beginner/profile-lookup_mariahlightfoot.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * write your solution
+ * make sure it works!
+ * stage the file: git add .
+ * commit the file: git commit -m ""
+ * git push origin solution-2
+ * 
+ * now YOUR fork of the repo will have your solution-2 branch on it
+ * go to GitHub and open a pull request from the solution-2 branch to the fork master (my repo, top of the list)
+ * add a PR message describing your steps
+ */
