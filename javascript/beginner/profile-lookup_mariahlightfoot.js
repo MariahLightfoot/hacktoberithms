c049c0c032dbda79241091341eb60f1fe74d8aac
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-10-29 20:17:38 -0500

    focusing on confirming that the name is is an actual contact's firstName. function works when using Akira as an example, but does not work if I use the other first names. Looking now.

--- javascript/beginner/profile-lookup_mariahlightfoot.js
+++ javascript/beginner/profile-lookup_mariahlightfoot.js
@@ -1,12 +1,50 @@
-/**
- * 
- * write your solution
- * make sure it works!
- * stage the file: git add .
- * commit the file: git commit -m ""
- * git push origin solution-2
- * 
- * now YOUR fork of the repo will have your solution-2 branch on it
- * go to GitHub and open a pull request from the solution-2 branch to the fork master (my repo, top of the list)
- * add a PR message describing your steps
- */
+let contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name){
+    for (let i=0; i < contacts.length; i++){
+        if(name === contacts[i].firstName){
+            return "YAS";
+        } else {
+            return "No such contact";
+        }
+    }
+}
+  
+console.log(lookUpProfile("Harry"));
+//console.log(contacts[0].likes.length);
+
+
+// Change these values to test your function
+//look_up_profile("Akira", "likes")
+
+// if(field === contacts[i][field]){
+            //     console.log(`${field}`);
+            //     return contacts[i][field];
+            // } else {
+            //     return "No such property";
+            // }
